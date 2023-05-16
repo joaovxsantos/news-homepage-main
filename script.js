0703f6dc78b93afdbcdd1c63e7e6134e105d0261
João Vitor <joaovitor55670@gmail.com>
2023-05-16 18:10:05 -0300

    final

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const menu = document.querySelector('.menu');
 const menuIcon = document.querySelector('#menuIcon');
 const closeIcon = document.querySelector('#close')
+const main = document.querySelector('main');
 
 menuIcon.addEventListener('click', () => {
     menu.classList.add('active');
+    main.style = 'filter: grayscale(80%);'
 })
 
 closeIcon.addEventListener('click', () => {
     menu.classList.remove('active');
+    main.style = 'filter: none;'
 })
